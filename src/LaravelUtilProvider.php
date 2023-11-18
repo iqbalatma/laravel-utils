27d44d085252678e1862f3ac2b84669ef109d340
Iqbal Atma Muliawan <iqbalatma@gmail.com>
2023-11-18 17:10:20 +0700

    add command for publishing stub

--- src/LaravelUtilProvider.php
+++ src/LaravelUtilProvider.php
@@ -7,6 +7,7 @@ use Iqbalatma\LaravelUtils\Console\Command\GenerateAbstract;
 use Iqbalatma\LaravelUtils\Console\Command\GenerateEnum;
 use Iqbalatma\LaravelUtils\Console\Command\GenerateInterface;
 use Iqbalatma\LaravelUtils\Console\Command\GenerateTrait;
+use Iqbalatma\LaravelUtils\Console\Command\PublishStub;
 
 class LaravelUtilProvider extends ServiceProvider
 {
@@ -18,8 +19,8 @@ class LaravelUtilProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/Config/utils.php', 'utils');
 
         $this->publishes([
-            __DIR__.'/Console/Stubs/enum.stub' => base_path('stubs/laravel-utils/enum.stub'),
-        ], 'stub_enum');
+            __DIR__.'/Config/utils.php' => config_path('utils.php'),
+        ], "config");
     }
 
     /**
@@ -33,11 +34,8 @@ class LaravelUtilProvider extends ServiceProvider
                 GenerateEnum::class,
                 GenerateInterface::class,
                 GenerateTrait::class,
+                PublishStub::class,
             ]);
         }
-
-        $this->publishes([
-            __DIR__.'/Config/utils.php' => config_path('utils.php'),
-        ]);
     }
 }
